90e1c3a368eee1cf98f9654fc8e8decce43d7ca6
汪潼 <5400111+getter_wang@user.noreply.gitee.com>
2025-03-05 14:45:22 +0800

    静态文件，通过id获取聊天内容接口

--- router/chat_info.js
+++ router/chat_info.js
@@ -1,8 +1,9 @@
-const Chat = require("../db/model/chatInfo");
+const ChatInfo = require("../db/model/chatInfo");
 const express = require("express");
 const vertoken = require("../token/index");
 const { successWithData, fail } = require("../utils/result");
 const router = express.Router();
+const {Op} = require("sequelize");
 
 router.post("/chat/chatrecord", async (req, res) => {
   let token = req.headers.authorization;
@@ -10,15 +11,17 @@ router.post("/chat/chatrecord", async (req, res) => {
   let to_id = req.body.id;
   if (tokenUser) {
     let { id } = tokenUser;
+    console.log(id,to_id);
     try {
-      let chatList = await Chat.findAll({
-        where: { from_id: id, to_id: to_id },
-        order: {
-          create_time: "desc",
-        },
+      let chatList = await ChatInfo.findAll({
+
+        where: { [Op.or]:[{from_id: id, to_id: to_id}, {from_id: to_id, to_id: id}] },
+        order: [["created_time","ASC"]],
       });
       res.send(successWithData(chatList));
     } catch (err) {
+      console.log(err);
+      
       res.send(fail(err));
     }
   }
